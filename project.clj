607fd2adfe35f8aaf02139151d85ec039b3cab33
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-08-31 11:01:29 +0700

    add url to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject bootstrap "0.0.1-SNAPSHOT"
   :description "Using Twitter Bootstrap with Hiccup and Noir"
+  :url "http://github.com/myguidingstar/bootstrap"
   :dependencies [[org.clojure/clojure "1.3.0"]])
