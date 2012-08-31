53f41dff7c0e9b4d9950c03d6f78ef933e6647cc
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-08-31 10:56:04 +0700

    remove some files added by mistake

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject bootstrap "0.0.1-SNAPSHOT"
-  :description "Using Twitter Bootstrap with hiccup"
+  :description "Using Twitter Bootstrap with Hiccup and Noir"
   :dependencies [[org.clojure/clojure "1.3.0"]])
