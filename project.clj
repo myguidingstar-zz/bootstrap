a2ec65244f5c24ad377dd9220175f4d90fce94aa
Hoàng Minh Thắng <hoangminhthang@ktqd.org>
2012-09-12 10:39:59 +0700

    change description

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject bootstrap "0.0.1-SNAPSHOT"
-  :description "Using Twitter Bootstrap with Hiccup and Noir"
+  :description "Twitter Bootstrap resources and helpers"
   :url "http://github.com/myguidingstar/bootstrap"
   :dependencies [[org.clojure/clojure "1.3.0"]])
